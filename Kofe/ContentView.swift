b32f3fccc1579328e1e6dbfd009edac3820fe23a
Richard Privalov <riprivalov@berkeley.edu>
2021-04-03 00:26:32 -0700

    Initial Commit

---
+++ Kofe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kofe
+//
+//  Created by muca on 4/3/21.
+//  Copyright © 2021 Nightowls. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
